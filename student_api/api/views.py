999fff06069c1f1c481f30c968066e178be7ca7f
Aman <amanrc28032@gmail.com>
2023-04-29 23:56:26 +0530

    my first django project

--- student_api/api/views.py
+++ student_api/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import Response
+from rest_framework.generics import {
+    ListAPIView,
+    RetrieverAPIView,
+    updateAPIView,
+}
 
 # Create your views here.
